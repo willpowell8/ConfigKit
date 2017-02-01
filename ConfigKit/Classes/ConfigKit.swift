bb3fc037ca405c6b693435e42cdfa04944bce36f
Will Powell <willpowell8@gmail.com>
2017-02-01 16:20:59 +0000

    initial commit

---
+++ ConfigKit/Classes/ConfigKit.swift
@@ -0,0 +1,33 @@
+//
+//  Configurationkit.swift
+//  Pods
+//
+//  Created by Will Powell on 01/02/2017.
+//
+//
+
+import Foundation
+
+public class ConfigKit {
+    
+    public static var baseURL = "https://www.localizationkit.com/config/";
+    
+    public static func getConfig(str:String, _ completion: @escaping (NSDictionary) -> Swift.Void){
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let urlString = "\(baseURL)\(str)"
+        let url = URL(string: urlString as String)
+        session.dataTask(with: url!) {
+            (data, response, error) in
+            if (response as? HTTPURLResponse) != nil {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary
+                    completion(json!)
+                } catch {
+                    print("error serializing JSON: \(error)")
+                }
+                
+            }
+            }.resume()
+    }
+}
